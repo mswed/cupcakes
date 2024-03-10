3de86a74d5ebee9f5e6b168a0f084d71feaed609
Moshe Swed <mswed@beapot.com>
2024-03-10 19:38:23 -0400

    step 4

--- tests.py
+++ tests.py
@@ -27,6 +27,13 @@ CUPCAKE_DATA_2 = {
     "image": "http://test.com/cupcake2.jpg"
 }
 
+CUPCAKE_DATA_3 = {
+    "flavor": "TestFlavor3",
+    "size": "TestSize3",
+    "rating": 2,
+    "image": "http://test.com/cupcake3.jpg"
+}
+
 
 class CupcakeViewsTestCase(TestCase):
     """Tests for views of API."""
@@ -106,3 +113,32 @@ class CupcakeViewsTestCase(TestCase):
             })
 
             self.assertEqual(Cupcake.query.count(), 2)
+
+    def test_update_cupcake(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/{self.cupcake.id}"
+            resp = client.patch(url, json=CUPCAKE_DATA_3)
+
+            self.assertEqual(resp.status_code, 200)
+            data = resp.json
+            self.assertEqual(data, {
+                "cupcake": {
+                    "id": self.cupcake.id,
+                    "flavor": "TestFlavor3",
+                    "size": "TestSize3",
+                    "rating": 2,
+                    "image": "http://test.com/cupcake3.jpg"
+                }
+            })
+
+    def test_delete_cupcake(self):
+        with app.test_client() as client:
+            print(len(Cupcake.query.all()))
+
+            url = f"/api/cupcakes/{self.cupcake.id}"
+            resp = client.delete(url)
+
+            self.assertEqual(resp.status_code, 200)
+            data = resp.json
+            self.assertEqual(data, {'message': 'DELETED'})
+            self.assertEqual(len(Cupcake.query.all()), 0)
